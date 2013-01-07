a4240c84ed4e61f7ce5a551b4dd76342e8401d85
Mathias Fussenegger <f.mathias@zignar.net>
2013-01-08 00:27:57 +0100

    add bootstrap.sh and clean.sh

---
+++ bootstrap.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# folders
+ln -s ${PWD}/config/i3status $HOME/.config/i3status
+ln -s ${PWD}/config/systemd $HOME/.config/systemd
+ln -s ${PWD}/i3 $HOME/.i3
+ln -s ${PWD}/mplayer $HOME/.mplayer
+ln -s ${PWD}/.vim $HOME/.vim
+ln -s ${PWD}/.zprezto $HOME/.zprezto
+
+# files
+ln -s ${PWD}/config/myremote $HOME/.config/myremote
+ln -s ${PWD}/Xresources $HOME/.Xresources
+ln -s ${PWD}/gitconfig $HOME/.gitconfig
+ln -s ${PWD}/gitignore $HOME/.gitignore
+ln -s ${PWD}/inputrc $HOME/.inputrc
+ln -s ${PWD}/lircrc $HOME/.lircrc
+ln -s ${PWD}/pentadactylrc $HOME/.pentadactylrc
+ln -s ${PWD}/pythonrc.py $HOME/.pythonrc.py
+ln -s ${PWD}/screenrc $HOME/.screenrc
+ln -s ${PWD}/vimrc $HOME/.vimrc
+ln -s ${PWD}/xinitrc $HOME/.xinitrc
+ln -s ${PWD}/.zprezto/runcoms/zlogin $HOME/.zlogin
+ln -s ${PWD}/.zprezto/runcoms/zlogout $HOME/.zlogout
+ln -s ${PWD}/.zprezto/runcoms/zpreztorc $HOME/.zpreztorc
+ln -s ${PWD}/.zprezto/runcoms/zprofile $HOME/.zprofile
+ln -s ${PWD}/.zprezto/runcoms/zshenv $HOME/.zshenv
+ln -s ${PWD}/.zprezto/runcoms/zshrc $HOME/.zshrc
+ln -s ${PWD}/zshrc.local $HOME/.zshrc.local
+ln -s ${PWD}/nose.cfg $HOME/nose.cfg
+
+echo "PATH=\$PATH:${PWD}/bin" > $HOME/.zshrc.local.dyn
---
+++ clean.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# dir symlinks
+rm -f $HOME/.config/i3status
+rm -f $HOME/.config/systemd
+rm -f $HOME/.i3
+rm -f $HOME/.mplayer
+rm -f $HOME/.vim
+rm -f $HOME/.zprezto
+
+# file symlinks
+rm -f $HOME/.config/myremote
+rm -f $HOME/.Xresources
+rm -f $HOME/.gitconfig
+rm -f $HOME/.gitignore
+rm -f $HOME/.gitmodules
+rm -f $HOME/.inputrc
+rm -f $HOME/.lircrc
+rm -f $HOME/.pentadactylrc
+rm -f $HOME/.pythonrc.py
+rm -f $HOME/.screenrc
+rm -f $HOME/.vimrc
+rm -f $HOME/.xinitrc
+rm -f $HOME/.zlogin
+rm -f $HOME/.zlogout
+rm -f $HOME/.zpreztorc
+rm -f $HOME/.zprofile
+rm -f $HOME/.zshenv
+rm -f $HOME/.zshrc
+rm -f $HOME/.zshrc.local
+rm -f $HOME/nose.cfg
