de496b91d52d51c39e6ecba473ba0239e7bf1a40
Mathias Fussenegger <f.mathias@zignar.net>
2013-07-04 19:25:02 +0200

    increase volume while playing the alarm

--- bin/go2bed.py
+++ bin/go2bed.py
@@ -3,9 +3,14 @@
 
 import time
 import sys
+import re
 
 from subprocess import call
 from datetime import datetime, timedelta
+from multiprocessing import Process
+
+from sh import amixer
+
 
 now = datetime.now()
 
@@ -27,6 +32,24 @@ def get_sleep_cycles():
     return sleep_cycles
 
 
+def get_vol():
+    output = str(amixer('sget', 'Master'))
+    match = re.findall('Front Left: .*\[(\d+)%\].*', output)
+    if match:
+        return int(match[0])
+    return 0
+
+
+def inc_vol():
+    amixer('-q', 'set', 'Master', '2%+', 'unmute')
+
+
+def max_volume():
+    while get_vol() < 100:
+        inc_vol()
+        time.sleep(10)
+
+
 def main():
     if len(sys.argv) >= 2:
         sleep_cycles = int(sys.argv[1])
@@ -36,7 +59,8 @@ def main():
     # 20 minutes to fall asleep
     minutes_to_sleep = (sleep_cycles * 90 + 20)
     print(minutes_to_sleep)
-    print('wakeup at {0:%H:%M}'.format(now + timedelta(minutes=minutes_to_sleep)))
+    print('wakeup at {0:%H:%M}'.format(
+        now + timedelta(minutes=minutes_to_sleep)))
 
     try:
         time.sleep(5)
@@ -46,15 +70,16 @@ def main():
     seconds_to_sleep = minutes_to_sleep * 60
 
     call('sudo rtcwake -m mem -s {0}'.format(seconds_to_sleep), shell=True)
+    p = Process(target=max_volume)
+    p.start()
     try:
         call('mplayer ~/workspace/audio/wakeup/*', shell=True)
     except KeyboardInterrupt:
+        p.terminate()
         sys.exit(0)
 
     call(['mpc', 'play'])
-
     time.sleep(15)
-
     call(['sudo', 'rc.d', 'restart', 'fancontrol'])
 
 
