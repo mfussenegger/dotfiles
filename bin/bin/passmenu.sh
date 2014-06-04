c816ca795d70f42a2933d38b3786d3c9065c72d3
Mathias Fussenegger <f.mathias@zignar.net>
2014-06-04 22:54:17 +0200

    add dmenu pass script and i3 hotkey to launch it

---
+++ bin/bin/passmenu.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+shopt -s nullglob globstar
+
+prefix=${PASSWORD_STORE_DIR-~/.password-store}
+password_files=( "$prefix"/**/*.gpg )
+password_files=( "${password_files[@]#"$prefix"/}" )
+password_files=( "${password_files[@]%.gpg}" )
+
+password=$(printf '%s\n' "${password_files[@]}" | dmenu "$@")
+
+[[ -n $password ]] && pass show -c "$password" 2>/dev/null
