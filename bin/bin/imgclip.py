5beeb4856deecb9a64318dcf3401772f096948e7
Mathias Fussenegger <f.mathias@zignar.net>
2014-10-21 21:03:26 +0200

    update imgclip.py for python3

--- bin/bin/imgclip.py
+++ bin/bin/imgclip.py
@@ -1,22 +1,21 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import gtk
+from gi.repository import Gtk, Gdk
 import sys
 
 count = 0
 
-
 def handle_owner_change(clipboard, event):
     global count
-    print 'clipboard.owner-change(%r, %r)' % (clipboard, event)
+    print('clipboard.owner-change(%r, %r)' % (clipboard, event))
     count += 1
     if count > 1:
         sys.exit(0)
 
-image = gtk.gdk.pixbuf_new_from_file(sys.argv[1])
-clipboard = gtk.clipboard_get()
+image = Gtk.Image.new_from_file(sys.argv[1])
+clipboard = Gtk.Clipboard.get(Gdk.SELECTION_CLIPBOARD)
 clipboard.connect('owner-change', handle_owner_change)
-clipboard.set_image(image)
+clipboard.set_image(image.get_pixbuf())
 clipboard.store()
-gtk.main()
+Gtk.main()
