83f7a42a2c4888f50841a6248172ca3e77051067
Mathias Fussenegger <f.mathias@zignar.net>
2016-02-23 15:12:34 +0100

    update MyAntigen.hs

--- shell/.zsh/MyAntigen.hs
+++ shell/.zsh/MyAntigen.hs
@@ -16,9 +16,12 @@ bundles =
   [ bundle "mafredri/zsh-async"
   , bundle "sindresorhus/pure"
   , bundle "zsh-users/zsh-syntax-highlighting"
-  , bundle "zsh-users/zsh-history-substring-search"
+  , (bundle "zsh-users/zsh-history-substring-search")
+    { sourcingStrategy = filePathsSourcingStrategy [ "zsh-history-substring-search.zsh" ] }
   , bundle "zsh-users/zsh-completions"
-  , (bundle "b4b4r07/enhancd") { sourcingStrategy = antigenSourcingStrategy }
+  , (bundle "b4b4r07/enhancd")
+    { sourcingStrategy = antigenSourcingStrategy
+    , sourcingLocations = ["zsh"] }
   ]
 
 config = defaultConfig { plugins = bundles }
