9ad35d594433fafdef421ce2589f907a5583511f
Mathias Fussenegger <f.mathias@zignar.net>
2016-10-14 13:01:05 +0200

    Make i3pystatus config more dynamic

--- i3/.config/i3status/config.py
+++ i3/.config/i3status/config.py
@@ -1,41 +1,41 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 from i3pystatus import Status
+try:
+    import netifaces
+except ImportError:
+    netifaces = None
 import os
 
 
-IS_NOTEBOOK = len(os.listdir('/sys/class/power_supply')) > 0
-
 status = Status(standalone=True)
-
-status.register('clock',
-                format='%H:%M %d.%m',
-                interval=30)
+status.register('clock', format='%H:%M %d.%m', interval=30)
 status.register('load')
 status.register('temp')
 
-if IS_NOTEBOOK:
+
+batteries = [b for b in os.listdir('/sys/class/power_supply')
+             if not b.startswith('hid')]
+if batteries:
     status.register('battery',
                     alert=True,
                     format='Batt: {percentage_design:.0f}% {status}')
-    status.register('network',
-                    interface='wlan0',
-                    format_up='{v4} {essid} {quality:03.0f}%',
-                    interval=5)
-else:
-    status.register('network',
-                    interface='br0',
-                    format_up='{v4}',
-                    interval=5)
-
-status.register('network',
-                interface='eth0',
-                format_up='{v4}',
-                format_down='{interface}',
-                interval=5)
-status.register('pulseaudio', format='Volume: {volume}')
-status.register('openvpn', vpn_name='lovely', format='VPN: {vpn_name}')
 
 
+if netifaces:
+    hidden_ifaces = set(['lo', 'docker0'])
+    for iface in netifaces.interfaces():
+        if iface in hidden_ifaces:
+            continue
+        if iface.startswith('w'):
+            fmt = '{v4} {essid} {quality:03.0f}%'
+        else:
+            fmt = '{v4}'
+        status.register('network', interface=iface, format_up=fmt, interval=5)
+
+
+status.register('pulseaudio', format='♪ {volume}')
+for conf in os.listdir('/etc/openvpn'):
+    name, ext = os.path.splitext(conf)
+    if ext != '.conf':
+        continue
+    status.register('openvpn', vpn_name=name, format='VPN: {vpn_name}')
 status.run()
