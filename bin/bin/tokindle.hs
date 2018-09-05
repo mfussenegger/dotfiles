010c98ae1177fb0e4a24a2cf4b2dfa5e87c86618
Mathias Fussenegger <f.mathias@zignar.net>
2018-09-05 20:19:52 +0200

    Update stack resolver to lts-12.9

--- bin/bin/tokindle.hs
+++ bin/bin/tokindle.hs
@@ -1,5 +1,5 @@
 #!/usr/bin/env stack
--- stack script --resolver lts-12.7 --package "process directory temporary split"
+-- stack script --resolver lts-12.9 --package "process directory temporary split"
 
 import           Data.Char              (isSpace)
 import           Data.List.Split        (splitOn)
