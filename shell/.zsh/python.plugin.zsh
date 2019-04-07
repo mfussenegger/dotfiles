c7f18856b218d0a2ed25ffa1c96b4f42f0bfa3dd
Mathias Fussenegger <f.mathias@zignar.net>
2019-04-07 16:39:42 +0200

    zsh/python: Remove new_buildout

--- shell/.zsh/python.plugin.zsh
+++ shell/.zsh/python.plugin.zsh
@@ -13,32 +13,3 @@ venv() {
         fi
     fi
 }
-
-
-new_buildout() {
-    wget http://downloads.buildout.org/2/bootstrap.py
-    echo "[versions]" > versions.cfg
-    cat > buildout.cfg << EOF
-[buildout]
-develop = .
-extends = versions.cfg
-versions = versions
-show-picked-versions = true
-parts = scripts
-        test
-
-
-[scripts]
-recipe = zc.recipe.egg:script
-interpreter = py
-eggs = wheel
-
-[test]
-relative-paths = true
-recipe = zc.recipe.testrunner
-defaults = ['--auto-color']
-EOF
-    python3 -m venv .venv
-    .venv/bin/python -m ensurepip
-    .venv/bin/python bootstrap.py
-}
