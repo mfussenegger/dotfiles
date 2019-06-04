a5fc33d9ad8615bc994d7b2bcf28af805936d449
Mathias Fussenegger <f.mathias@zignar.net>
2019-06-04 22:41:56 +0200

    Extract java lsp invocation into a bash script

---
+++ bin/bin/java-lsp.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+JAR="$HOME/dev/eclipse/eclipse.jdt.ls/org.eclipse.jdt.ls.product/target/repository/plugins/org.eclipse.equinox.launcher_*.jar"
+GRADLE_HOME=$HOME/gradle /usr/lib/jvm/java-11-openjdk/bin/java \
+  -Declipse.application=org.eclipse.jdt.ls.core.id1 \
+  -Dosgi.bundles.defaultStartLevel=4 \
+  -Declipse.product=org.eclipse.jdt.ls.core.product \
+  -Dlog.protocol=true \
+  -Dlog.level=ALL \
+  -noverify \
+  -Xmx1G \
+  -jar $(echo "$JAR") \
+  -configuration "$HOME/dev/eclipse/eclipse.jdt.ls/org.eclipse.jdt.ls.product/target/repository/config_linux" \
+  -data "$HOME/.local/share/eclipse" \
+  --add-modules=ALL-SYSTEM \
+  --add-opens java.base/java.util=ALL-UNNAMED \
+  --add-opens java.base/java.lang=ALL-UNNAMED
