9dc3f80646074a152500e9a7604915e6a127250c
Mathias Fussenegger <f.mathias@zignar.net>
2020-04-26 14:52:05 +0200

    vim: Use built-in diagnostics_by_buf and delay only show_line_diagnostics

--- vim/.config/nvim/lua/lsp-diagnostics.lua
+++ vim/.config/nvim/lua/lsp-diagnostics.lua
@@ -1,86 +1,29 @@
 local myutil = require 'util'
 local api = vim.api
-local protocol = vim.lsp.protocol
-local jdtls = require'jdtls'
-
--- {<bufnr>: {<lineNr>: {diagnostics}}}
-local sign_ns = 'vim_lsp_signs'
 local timer = nil
 local M = {}
 
 local updatedDiagnostics = false
 local nextDiag = 0
 
-M.diagnostics_by_buffer = {}
 
-
-local function diagnostics_to_items(bufnr, buf_diagnostics)
-    local items = {}
-    if not buf_diagnostics then return items end
-    for linenr, diagnostics in pairs(buf_diagnostics) do
-        if #diagnostics > 0 then
-            local d = diagnostics[1]
-            table.insert(items, {
-                bufnr = bufnr,
-                lnum = linenr + 1,
-                vcol = 1,
-                col = d.range.start.character + 1,
-                text = d.message
-            })
-        end
+local function diagnostics_to_items(bufnr, diagnostics)
+  local items = {}
+  if not diagnostics then return items end
+  local diagnostics_by_line = vim.lsp.util.diagnostics_group_by_line(diagnostics)
+  for linenr, line_diagnostics in pairs(diagnostics_by_line) do
+    if #line_diagnostics > 0 then
+      local d = line_diagnostics[1]
+      table.insert(items, {
+        bufnr = bufnr,
+        lnum = linenr + 1,
+        vcol = 1,
+        col = d.range.start.character + 1,
+        text = d.message
+      })
     end
-    return items
-end
-
-
-local function set_signs(bufnr, buf_diagnostics)
-    vim.fn.sign_unplace(sign_ns, {buffer=bufnr})
-    if not buf_diagnostics then return end
-    vim.fn.sign_define('LspDiagnosticsErrorSign', {text='E', texthl='LspDiagnosticsError', linehl='', numhl=''})
-    vim.fn.sign_define('LspDiagnosticsWarningSign', {text='W', texthl='LspDiagnosticsWarning', linehl='', numhl=''})
-    vim.fn.sign_define('LspDiagnosticsInformationSign', {text='I', texthl='LspDiagnosticsInformation', linehl='', numhl=''})
-    vim.fn.sign_define('LspDiagnosticsHintSign', {text='H', texthl='LspDiagnosticsHint', linehl='', numhl=''})
-    local severity_map = {
-        [protocol.DiagnosticSeverity.Error] = "LspDiagnosticsErrorSign";
-        [protocol.DiagnosticSeverity.Warning] = "LspDiagnosticsWarningSign";
-        [protocol.DiagnosticSeverity.Information] = "LspDiagnosticsInformationSign";
-        [protocol.DiagnosticSeverity.Hint] = "LspDiagnosticsHintSign";
-    }
-    for _, diagnostics in pairs(buf_diagnostics) do
-        if #diagnostics > 0 then
-            local d = diagnostics[1]
-            vim.fn.sign_place(
-                0,
-                sign_ns,
-                severity_map[d.severity],
-                bufnr,
-                { lnum = (d.range.start.line + 1) }
-            )
-        end
-    end
-end
-
-
-local function save_diagnostics(bufnr, diagnostics, uri)
-    if not diagnostics then return end
-    jdtls.save_diagnostics(bufnr, diagnostics)
-    if not M.diagnostics_by_buffer[bufnr] then
-        api.nvim_buf_attach(bufnr, false, {
-            on_detach = function(b) M.diagnostics_by_buffer[b] = nil end
-        })
-    end
-    local buf_diagnostics = {}
-    M.diagnostics_by_buffer[bufnr] = buf_diagnostics
-    for _, diagnostic in ipairs(diagnostics) do
-        local start = diagnostic.range.start
-        local line_diagnostics = buf_diagnostics[start.line]
-        if not line_diagnostics then
-            line_diagnostics = {}
-            buf_diagnostics[start.line] = line_diagnostics
-        end
-        diagnostic.uri = uri
-        table.insert(line_diagnostics, diagnostic)
-    end
+  end
+  return items
 end
 
 
@@ -92,77 +35,70 @@ function M.publishDiagnostics(_, _, result)
         myutil.err_message("LSP.publishDiagnostics: Couldn't find buffer for ", uri)
         return
     end
+    vim.lsp.util.buf_clear_diagnostics(bufnr)
     if not api.nvim_buf_is_loaded(bufnr) then
         return
     end
-    save_diagnostics(bufnr, result.diagnostics, result.uri)
+    for _, diagnostic in ipairs(result.diagnostics) do
+      diagnostic.severity = diagnostic.severity or vim.lsp.protocol.DiagnosticSeverity.Error
+      diagnostic.uri = uri
+    end
+    vim.lsp.util.buf_diagnostics_save_positions(bufnr, result.diagnostics)
+    vim.lsp.util.buf_diagnostics_signs(bufnr, result.diagnostics)
+    vim.fn.setloclist(0, {}, ' ', {
+      title = 'Language Server';
+      items = diagnostics_to_items(bufnr, result.diagnostics)
+    })
     updatedDiagnostics = true
     M.show_diagnostics()
 end
 
 
 function M.show_diagnostics()
+  if timer then
+    timer:stop()
+    timer:close()
+    timer = nil
+  end
+  timer = vim.loop.new_timer()
+  timer:start(450, 0, vim.schedule_wrap(function()
     if timer then
-        timer:stop()
-        timer:close()
-        timer = nil
+      timer:close()
+      timer = nil
     end
-    timer = vim.loop.new_timer()
-    timer:start(150, 0, vim.schedule_wrap(function()
-        if timer then
-            timer:close()
-            timer = nil
-        end
-
-        local bufnr = api.nvim_get_current_buf()
-        local buf_diagnostics = M.diagnostics_by_buffer[bufnr]
-        if buf_diagnostics and api.nvim_get_mode()['mode'] == 'n' then
-            local row = api.nvim_win_get_cursor(0)[1]
-            local d = buf_diagnostics[row - 1]
-            if d and #d > 0 and d[1].message then
-                print(d[1].message)
-            end
-        end
-
-        if not updatedDiagnostics then
-            return
-        end
-        updatedDiagnostics = false
-        nextDiag = 0
-
-        vim.fn.setloclist(0, {}, ' ', {
-            title = 'Language Server';
-            items = diagnostics_to_items(bufnr, buf_diagnostics)
-        })
-        set_signs(bufnr, buf_diagnostics)
-    end))
+    if api.nvim_get_mode()['mode'] == 'n' then
+      vim.lsp.util.show_line_diagnostics()
+    end
+    if updatedDiagnostics then
+      updatedDiagnostics = false
+      nextDiag = 0
+    end
+  end))
 end
 
 
 local function diagnostics_sorted_by_severity()
-    local all_diagnostics = {}
-    for _, buf_diagnostics in pairs(M.diagnostics_by_buffer) do
-        for _, line_diagnostics in pairs(buf_diagnostics) do
-            local last_col = nil
-            for _, d in pairs(line_diagnostics) do
-                local fname = vim.uri_to_fname(d.uri)
-                local stat = vim.loop.fs_stat(fname)
-                local is_dir = stat and stat.type == 'directory'
-                if not is_dir and (last_col == nil or last_col ~= d.range.start.character) then
-                    last_col = d.range.start.character
-                    table.insert(all_diagnostics, d)
-                end
-            end
-        end
+  local all_diagnostics = {}
+  for _, diagnostics in pairs(vim.lsp.util.diagnostics_by_buf) do
+    local last_col = nil
+    for _, d in pairs(diagnostics) do
+      local fname = vim.uri_to_fname(d.uri)
+      local stat = vim.loop.fs_stat(fname)
+      local is_dir = stat and stat.type == 'directory'
+      if not is_dir and (last_col == nil or last_col ~= d.range.start.character) then
+        last_col = d.range.start.character
+        table.insert(all_diagnostics, d)
+      end
     end
-    table.sort(all_diagnostics, function(a, b)
-        if a.severity == b.severity then
-            return a.range.start.line < b.range.start.line
-        else
-            return a.severity < b.severity
-        end
-    end)
-    return all_diagnostics
+  end
+  table.sort(all_diagnostics, function(a, b)
+    if a.severity == b.severity then
+      return a.range.start.line < b.range.start.line
+    else
+      return a.severity < b.severity
+    end
+  end)
+  return all_diagnostics
 end
 
 
