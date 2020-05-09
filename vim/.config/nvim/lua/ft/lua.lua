ca1d3bb9cc7b687247cecf77a143c1d2b1b51fc6
Mathias Fussenegger <f.mathias@zignar.net>
2020-05-09 12:02:35 +0200

    vim: Use tree-sitter highlighting for lua

---
+++ vim/.config/nvim/lua/ft/lua.lua
@@ -0,0 +1,73 @@
+-- Copied this from https://github.com/nvim-treesitter/nvim-treesitter/tree/master/queries/lua
+local query = [[
+;;; Highlighting for lua
+
+;;; Builtins
+;; Keywords
+"local" @keyword
+"if" @conditional
+"then" @conditional
+"else" @conditional
+"elseif" @conditional
+"end" @keyword
+"return" @keyword
+"do" @repeat
+"while" @repeat
+"repeat" @repeat
+"for" @repeat
+(break_statement) @keyword
+"goto" @keyword
+
+;; Operators
+"~=" @operator
+"==" @operator
+"<=" @operator
+">=" @operator
+"not" @operator
+"and" @operator
+"or" @operator
+"<" @operator
+">" @operator
+
+"+" @operator
+"-" @operator
+"%" @operator
+"/" @operator
+"//" @operator
+"*" @operator
+"^" @operator
+"&" @operator
+"~" @operator
+"|" @operator
+">>" @operator
+"<<" @operator
+".." @operator
+"#" @operator
+
+;; Constants
+(false) @boolean
+(true) @boolean
+(nil) @constant.builtin
+(spread) @constant ;; "..."
+
+;; Nodes
+(function "function" @function "end" @function)
+(function_definition "function" @function "end" @function)
+(local_function "function" @function "end" @function)
+(table "{" @constructor "}" @constructor)
+(comment) @comment
+(string) @string
+(number) @number
+(label_statement) @label
+
+;; Error
+(ERROR) @Error
+]]
+
+local M = {}
+
+function M.attach()
+  vim.treesitter.TSHighlighter.new(query)
+end
+
+return M
