d0029a517294e71e1fd4ef15e5f71cd4721ccf4a
Mathias Fussenegger <f.mathias@zignar.net>
2020-05-09 11:38:43 +0200

    vim: Read tree-sitter highlights query from checked out file

---
+++ vim/.config/nvim/lua/ft/java.lua
@@ -0,0 +1,10 @@
+local M = {}
+
+local highlights = os.getenv('HOME') .. '/dev/tree-sitter/tree-sitter-java/queries/highlights.scm'
+local query = table.concat(vim.fn.readfile(highlights), '\n')
+
+function M.attach()
+  vim.treesitter.TSHighlighter.new(query)
+end
+
+return M
