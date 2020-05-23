097354139919a733f0c668957549b3a5575d65ce
Mathias Fussenegger <f.mathias@zignar.net>
2020-05-23 22:48:32 +0200

    Update java to 14

--- bin/bin/java-lsp.sh
+++ bin/bin/java-lsp.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
 JAR="$HOME/dev/eclipse/eclipse.jdt.ls/org.eclipse.jdt.ls.product/target/repository/plugins/org.eclipse.equinox.launcher_*.jar"
-GRADLE_HOME=$HOME/gradle /usr/lib/jvm/java-13-openjdk/bin/java \
+GRADLE_HOME=$HOME/gradle /usr/lib/jvm/java-14-openjdk/bin/java \
   -Declipse.application=org.eclipse.jdt.ls.core.id1 \
   -Dosgi.bundles.defaultStartLevel=4 \
   -Declipse.product=org.eclipse.jdt.ls.core.product \
   -Dlog.protocol=true \
   -Dlog.level=ALL \
+  -Xms1g \
   -Xmx1G \
   -jar $(echo "$JAR") \
   -configuration "$HOME/dev/eclipse/eclipse.jdt.ls/org.eclipse.jdt.ls.product/target/repository/config_linux" \
