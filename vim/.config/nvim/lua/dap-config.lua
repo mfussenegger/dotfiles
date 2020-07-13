db49ac66ccf0c3577a1d0771d590b9292b3d09af
Mathias Fussenegger <f.mathias@zignar.net>
2020-07-13 19:45:27 +0200

    vim/dap: Add cpp adapter configuration

--- vim/.config/nvim/lua/dap-config.lua
+++ vim/.config/nvim/lua/dap-config.lua
@@ -32,3 +32,64 @@ dap.configurations.java = {
     port = 5005;
   },
 }
+
+
+dap.adapters.cpp = {
+  type = 'executable',
+  command = 'lldb-vscode',
+  env = {
+    LLDB_LAUNCH_FLAG_LAUNCH_IN_TTY = "YES"
+  },
+  name = "lldb"
+}
+
+dap.configurations.cpp = {
+  {
+    name = "Launch",
+    type = "cpp",
+    request = "launch",
+    program = function()
+      return vim.fn.input('Path to executable: ',  vim.fn.getcwd() .. '/')
+    end,
+    cwd = '${workspaceFolder}',
+    env = function()
+      local variables = {}
+      for k, v in pairs(vim.fn.environ()) do
+        table.insert(variables, string.format("%s=%s", k, v))
+      end
+      return variables
+    end,
+    stopOnEntry = false,
+    args = {}
+  },
+}
+
+vim.cmd [[
+    command! -complete=file -nargs=* DebugC lua require "dap-config".start_c_debugger({<f-args>}, "gdb")
+]]
+vim.cmd [[
+    command! -complete=file -nargs=* DebugRust lua require "dap-config".start_c_debugger({<f-args>}, "gdb", "rust-gdb")
+]]
+
+
+local M = {}
+local last_gdb_config
+
+M.start_c_debugger = function(args, mi_mode, mi_debugger_path)
+  if args and #args > 0 then
+    last_gdb_config = vim.deepcopy(dap.configurations.cpp[1])
+    last_gdb_config.name = args[1]
+    last_gdb_config.program = table.remove(args, 1)
+    last_gdb_config.args = args
+    last_gdb_config.MIMode = mi_mode or "gdb"
+    last_gdb_config.MIDebuggerPath = mi_debugger_path
+  end
+  if not last_gdb_config then
+    print('No binary to debug set! Use ":DebugC <binary> <args>" or ":DebugRust <binary> <args>"')
+    return
+  end
+  dap.run(last_gdb_config)
+  dap.repl.open()
+end
+
+return M
