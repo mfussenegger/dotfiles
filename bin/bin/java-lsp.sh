ef2cf60de8ae2c8553ee51486d7df261b6e8e53e
Mathias Fussenegger <f.mathias@zignar.net>
2020-10-13 20:30:23 +0200

    Bump java to 15

--- bin/bin/java-lsp.sh
+++ bin/bin/java-lsp.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 JAR="$HOME/dev/eclipse/eclipse.jdt.ls/org.eclipse.jdt.ls.product/target/repository/plugins/org.eclipse.equinox.launcher_*.jar"
-GRADLE_HOME=$HOME/gradle /usr/lib/jvm/java-14-openjdk/bin/java \
+GRADLE_HOME=$HOME/gradle $HOME/.local/jdks/jdk-15+36/bin/java \
   -Declipse.application=org.eclipse.jdt.ls.core.id1 \
   -Dosgi.bundles.defaultStartLevel=4 \
   -Declipse.product=org.eclipse.jdt.ls.core.product \
   -Dlog.protocol=true \
   -Dlog.level=ALL \
+  -XX:+UseShenandoahGC \
   -Xms1g \
-  -Xmx2G \
   -jar $(echo "$JAR") \
   -configuration "$HOME/dev/eclipse/eclipse.jdt.ls/org.eclipse.jdt.ls.product/target/repository/config_linux" \
   -data "$1" \
