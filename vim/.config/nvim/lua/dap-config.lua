2cb390711a90b8e3e59ab1b5ae1a7f7af33e4cb1
Mathias Fussenegger <f.mathias@zignar.net>
2020-12-24 11:16:18 +0100

    vim/dap: Add haskell debug configuration

--- vim/.config/nvim/lua/dap-config.lua
+++ vim/.config/nvim/lua/dap-config.lua
@@ -34,6 +34,28 @@ dap.configurations.go = {
   },
 }
 
+dap.adapters.haskell = {
+  type = 'executable';
+  command = 'haskell-debug-adapter';
+  args = {'--hackage-version=0.0.33.0'};
+}
+dap.configurations.haskell = {
+  {
+    type = 'haskell',
+    request = 'launch',
+    name = 'Debug',
+    workspace = '${workspaceFolder}',
+    startup = "${file}",
+    stopOnEntry = true,
+    logFile = vim.fn.stdpath('data') .. '/haskell-dap.log',
+    logLevel = 'WARNING',
+    ghciEnv = vim.empty_dict(),
+    ghciPrompt = "λ: ",
+    ghciInitialPrompt = "λ: ",
+    ghciCmd= "stack ghci --test --no-load --no-build --main-is TARGET --ghci-options -fprint-evld-with-show",
+  },
+}
+
 
 dap.adapters.cpp = {
   type = 'executable',
