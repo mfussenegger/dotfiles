30f084aae6ab074bf4d458e5719b76f718919e51
Mathias Fussenegger <f.mathias@zignar.net>
2021-03-14 10:59:36 +0100

    vim/lsp: Support :tag and :tselect with tagfunc

--- vim/.config/nvim/lua/lsp-ext.lua
+++ vim/.config/nvim/lua/lsp-ext.lua
@@ -312,44 +312,66 @@ function M.attach(client, bufnr)
 end
 
 
-function M.tagfunc(pattern, flags)
-  if flags ~= 'c' then
-    return vim.NIL
-  end
-  local params = vim.lsp.util.make_position_params()
-  local results_by_client, err = vim.lsp.buf_request_sync(0, 'textDocument/definition', params, 1000)
-  assert(not err, vim.inspect(err))
 
-  local results = {}
-  local add = function(range, uri)
+do
+  local function mk_tag_item(name, range, uri)
     local start = range.start
-    table.insert(results, {
-      name = pattern,
+    return {
+      name = name,
       filename = vim.uri_to_fname(uri),
       cmd = string.format(
         'call cursor(%d, %d)', start.line + 1, start.character + 1
       )
-    })
+    }
   end
-  for _, lsp_results in ipairs(results_by_client) do
-    -- result can be Location | Location[] | LocationLink[]
-    local result = lsp_results.result or {}
-    if result.range then
-      -- Location
-      add(result.range, result.uri)
-    else
-      for _, item in ipairs(result) do
-        if item.range then
-          -- Location
-          add(item.range, item.uri)
-        else
-          -- LocationLink
-          add(item.targetSelectionRange, item.targetUri)
+
+  local function query_definition(pattern)
+    local params = vim.lsp.util.make_position_params()
+    local results_by_client, err = vim.lsp.buf_request_sync(0, 'textDocument/definition', params, 1000)
+    assert(not err, vim.inspect(err))
+    local results = {}
+    local add = function(range, uri) table.insert(results, mk_tag_item(pattern, range, uri)) end
+    for _, lsp_results in ipairs(results_by_client) do
+      local result = lsp_results.result or {}
+      if result.range then              -- Location
+        add(result.range, result.uri)
+      else                              -- Location[] or LocationLink[]
+        for _, item in ipairs(result) do
+          if item.range then            -- Location
+            add(item.range, item.uri)
+          else                          -- LocationLink
+            add(item.targetSelectionRange, item.targetUri)
+          end
         end
       end
     end
+    return results
   end
-  return results
+
+  local function query_workspace_symbols(pattern)
+    local results_by_client, err = vim.lsp.buf_request_sync(0, 'workspace/symbol', { query = pattern }, 1000)
+    assert(not err, vim.inspect(err))
+    local results = {}
+    for _, symbols in ipairs(results_by_client) do
+      for _, symbol in ipairs(symbols.result or {}) do
+        local loc = symbol.location
+        local item = mk_tag_item(symbol.name, loc.range, loc.uri)
+        item.kind = vim.lsp.protocol.SymbolKind[symbol.kind] or 'Unknown'
+        table.insert(results, item)
+      end
+    end
+    return results
+  end
+
+  function M.tagfunc(pattern, flags)
+    if flags == 'c' then
+      return query_definition(pattern)
+    elseif flags == '' or flags == 'i' then
+      return query_workspace_symbols(pattern)
+    else
+      return vim.NIL
+    end
+  end
 end
 
 return M
