c3c5566b0447b49ff041da0498f518e9be61d536
Mathias Fussenegger <f.mathias@zignar.net>
2021-07-02 17:46:00 +0200

    Replace custom fzy quickfix/buffers with fzy.format_bufname override

--- vim/.config/nvim/lua/me/fzy.lua
+++ vim/.config/nvim/lua/me/fzy.lua
@@ -1,59 +1,14 @@
-local M = {}
 local fzy = require('fzy')
-local vfn = vim.fn
 local api = vim.api
 
 
--- Like fzy.actions.quickfix but with custom jdt:// URI handling
-function M.quickfix()
-  vim.cmd('cclose')
-  local items = vfn.getqflist()
-  fzy.pick_one(
-    items,
-    'Quickfix> ',
-    function(item)
-      local uri = vim.uri_from_bufnr(item.bufnr)
-      return require('me').format_uri(uri) .. ': ' .. item.text
-    end,
-    function(item)
-      if not item then return end
-      vfn.bufload(item.bufnr)
-      api.nvim_win_set_buf(0, item.bufnr)
-      api.nvim_win_set_cursor(0, {item.lnum, item.col - 1})
-      vim.cmd('normal! zvzz')
-    end
-  )
+fzy.format_bufname = function(bufnr)
+  local uri = vim.uri_from_bufnr(bufnr)
+  return require('me').format_uri(uri)
 end
 
 
--- Like fzy.actions.buffers but with custom jdt:// URI handling
-function M.buffers()
-  local bufs = vim.tbl_filter(
-    function(b)
-      return api.nvim_buf_is_loaded(b) and api.nvim_buf_get_option(b, 'buftype') ~= 'quickfix'
-    end,
-    api.nvim_list_bufs()
-  )
-  local format_bufname = function(b)
-    local fullname = api.nvim_buf_get_name(b)
-    local name
-    if #fullname == 0 then
-      name = '[No Name] (' .. api.nvim_buf_get_option(b, 'buftype') .. ')'
-    else
-      name= require('me').format_uri(vim.uri_from_bufnr(b))
-    end
-    local modified = api.nvim_buf_get_option(b, 'modified')
-    return modified and name .. ' [+]' or name
-  end
-  fzy.pick_one(bufs, 'Buffers> ', format_bufname, function(b)
-    if b then
-      api.nvim_set_current_buf(b)
-    end
-  end)
-end
-
-
-function M.emoji()
+function fzy.emoji()
   local lines = {}
   for line in io.lines(os.getenv('HOME') .. '/.config/dm/emoji.json') do
     table.insert(lines, line)
@@ -71,5 +26,4 @@ function M.emoji()
   )
 end
 
-
-return M
+return fzy
--- vim/.config/nvim/mappings.vim
+++ vim/.config/nvim/mappings.vim
@@ -70,14 +70,13 @@ augroup end
 
 
 if has('nvim-0.5')
-  lua fzy = require('fzy')
-  nnoremap <silent><leader>fq :lua require'me.fzy'.quickfix()<CR>
-  nnoremap <silent><leader>ff :lua fzy.execute('fd', fzy.sinks.edit_file)<CR>
-  nnoremap <silent><leader>fb :lua require'me.fzy'.buffers()<CR>
-  nnoremap <silent><leader>ft :lua fzy.try(fzy.actions.lsp_tags, fzy.actions.buf_tags)<CR>
-  nnoremap <silent><leader>fg :lua fzy.execute('git ls-files', fzy.sinks.edit_file)<CR>
-  nnoremap <silent><leader>f/ :lua fzy.actions.buf_lines()<CR>
-  inoremap <silent><c-e> <ESC>:lua require'me.fzy'.emoji()<CR>
+  nnoremap <silent><leader>fq :lua require('me.fzy').actions.quickfix()<CR>
+  nnoremap <silent><leader>ff :lua local fzy = require('me.fzy'); fzy.execute('fd', fzy.sinks.edit_file)<CR>
+  nnoremap <silent><leader>fb :lua require('me.fzy').actions.buffers()<CR>
+  nnoremap <silent><leader>ft :lua local fzy = require('me.fzy'); fzy.try(fzy.actions.lsp_tags, fzy.actions.buf_tags)<CR>
+  nnoremap <silent><leader>fg :lua local fzy = require('me.fzy'); fzy.execute('git ls-files', fzy.sinks.edit_file)<CR>
+  nnoremap <silent><leader>f/ :lua require('me.fzy').actions.buf_lines()<CR>
+  inoremap <silent><c-e> <ESC>:lua require('me.fzy').emoji()<CR>
 endif
 
 " gnupg
