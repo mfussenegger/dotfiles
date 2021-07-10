7a33c1588b4a5836b49aeb181c6bb2b85d73247e
Mathias Fussenegger <f.mathias@zignar.net>
2021-07-10 10:54:56 +0200

    vim/dap: Ensure `me.dap` is initialized on `jdtls.test*` mappings

--- vim/.config/nvim/lua/me/lsp/conf.lua
+++ vim/.config/nvim/lua/me/lsp/conf.lua
@@ -117,14 +117,17 @@ local function jdtls_on_attach(client, bufnr)
   local opts = { silent = true; }
   jdtls.setup_dap({hotcodereplace = 'auto'})
   jdtls.setup.add_commands()
-  api.nvim_buf_set_keymap(bufnr, "n", "<A-o>", "<Cmd>lua require'jdtls'.organize_imports()<CR>", opts)
-  api.nvim_buf_set_keymap(bufnr, "n", "<leader>df", "<Cmd>lua require'jdtls'.test_class()<CR>", opts)
-  api.nvim_buf_set_keymap(bufnr, "n", "<leader>dn", "<Cmd>lua require'jdtls'.test_nearest_method()<CR>", opts)
-  api.nvim_buf_set_keymap(bufnr, "v", "crv", "<Esc><Cmd>lua require('jdtls').extract_variable(true)<CR>", opts)
-  api.nvim_buf_set_keymap(bufnr, "n", "crv", "<Cmd>lua require('jdtls').extract_variable()<CR>", opts)
-  api.nvim_buf_set_keymap(bufnr, "v", "crm", "<Esc><Cmd>lua require('jdtls').extract_method(true)<CR>", opts)
-  api.nvim_buf_set_keymap(bufnr, "v", "crc", "<Esc><Cmd>lua require('jdtls').extract_constant(true)<CR>", opts)
-  api.nvim_buf_set_keymap(bufnr, "n", "crc", "<Cmd>lua require('jdtls').extract_constant()<CR>", opts)
+  local nnoremap = function(lhs, rhs)
+    api.nvim_buf_set_keymap(bufnr, 'n', lhs, rhs, opts)
+  end
+  nnoremap("<A-o>", "<Cmd>lua require'jdtls'.organize_imports()<CR>")
+  nnoremap("<leader>df", "<Cmd>lua require('me.dap'); require'jdtls'.test_class()<CR>")
+  nnoremap("<leader>dn", "<Cmd>lua require('me.dap'); require'jdtls'.test_nearest_method()<CR>")
+  nnoremap("crv", "<Esc><Cmd>lua require('jdtls').extract_variable(true)<CR>")
+  nnoremap("crv", "<Cmd>lua require('jdtls').extract_variable()<CR>")
+  nnoremap("crm", "<Esc><Cmd>lua require('jdtls').extract_method(true)<CR>")
+  nnoremap("crc", "<Esc><Cmd>lua require('jdtls').extract_constant(true)<CR>")
+  nnoremap("crc", "<Cmd>lua require('jdtls').extract_constant()<CR>")
 end
 
 
