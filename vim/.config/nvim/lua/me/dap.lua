80bdcd767b1eb11c50cb672d85413b86e2e6c9c8
Mathias Fussenegger <f.mathias@zignar.net>
2021-08-11 21:31:38 +0200

    vim/dap: Use pick_process function from nvim-dap

--- vim/.config/nvim/lua/me/dap.lua
+++ vim/.config/nvim/lua/me/dap.lua
@@ -20,35 +20,6 @@ local function setup_widgets()
 end
 
 
-local function pick_process()
-  local output = vim.fn.system({'ps', 'a'})
-  local lines = vim.split(output, '\n')
-  local procs = {}
-  for _, line in pairs(lines) do
-    -- output format
-    --    " 107021 pts/4    Ss     0:00 /bin/zsh <args>"
-    local parts = vim.fn.split(vim.fn.trim(line), ' \\+')
-    local pid = parts[1]
-    local name = table.concat({unpack(parts, 5)}, ' ')
-    if pid and pid ~= 'PID' then
-      pid = tonumber(pid)
-      if pid ~= vim.fn.getpid() then
-        table.insert(procs, { pid = tonumber(pid), name = name })
-      end
-    end
-  end
-  local choices = {'Select process'}
-  for i, proc in ipairs(procs) do
-    table.insert(choices, string.format("%d: pid=%d name=%s", i, proc.pid, proc.name))
-  end
-  local choice = vim.fn.inputlist(choices)
-  if choice < 1 or choice > #procs then
-    return nil
-  end
-  return procs[choice].pid
-end
-
-
 function M.setup()
   require('dap.ui').pick_one = require('fzy').pick_one
   setup_widgets()
@@ -235,7 +206,7 @@ function M.setup()
       name = "Attach to process",
       type = 'lldb',
       request = 'attach',
-      pid = pick_process,
+      pid = require('dap.utils').pick_process,
       args = {},
     },
   }
