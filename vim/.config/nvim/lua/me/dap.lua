f3ad14329e8df5f9fa6857f967d0611a2ccccc2f
Mathias Fussenegger <f.mathias@zignar.net>
2021-10-10 18:54:14 +0200

    Add codelldb

--- vim/.config/nvim/lua/me/dap.lua
+++ vim/.config/nvim/lua/me/dap.lua
@@ -169,7 +169,54 @@ function M.setup()
   dap.adapters.cppdbg = {
     type = 'executable',
     command = HOME .. '/apps/cpptools/extension/debugAdapters/OpenDebugAD7',
+    id = 'cppdbg',
   }
+  dap.adapters.codelldb = function(on_adapter)
+    local stdout = vim.loop.new_pipe(false)
+    local stderr = vim.loop.new_pipe(false)
+    local cmd = HOME ..  '/apps/codelldb/extension/adapter/codelldb'
+    local handle, pid_or_err
+    local opts = {
+      stdio = {nil, stdout, stderr},
+      detached = true,
+    }
+    handle, pid_or_err = vim.loop.spawn(cmd, opts, function(code)
+      stdout:close()
+      stderr:close()
+      handle:close()
+      if code ~= 0 then
+        print("codelldb exited with code", code)
+      end
+    end)
+    assert(handle, "Error running codelldb: " .. tostring(pid_or_err))
+    stdout:read_start(function(err, chunk)
+      assert(not err, err)
+      if chunk then
+        local port = chunk:match('Listening on port (%d+)')
+        if port then
+          vim.schedule(function()
+            on_adapter({
+              type = 'server',
+              host = '127.0.0.1',
+              port = port
+            })
+          end)
+        else
+          vim.schedule(function()
+            require("dap.repl").append(chunk)
+          end)
+        end
+      end
+    end)
+    stderr:read_start(function(err, chunk)
+      assert(not err, err)
+      if chunk then
+        vim.schedule(function()
+          require("dap.repl").append(chunk)
+        end)
+      end
+    end)
+  end
   dap.adapters.lldb = {
     type = 'executable',
     command = '/usr/bin/lldb-vscode',
@@ -189,7 +236,27 @@ function M.setup()
       end,
     },
     {
-      name = "Launch (integrated terminal)",
+      name = "Launch via cppdgb",
+      type = "cppdbg",
+      request = "launch",
+      program = function()
+        return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+      end,
+      cwd = '${workspaceFolder}',
+      args = {},
+    },
+    {
+      name = "Launch via codelldb",
+      type = "codelldb",
+      request = "launch",
+      program = function()
+        return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+      end,
+      cwd = '${workspaceFolder}',
+      args = {},
+    },
+    {
+      name = "Launch (lldb via integrated terminal)",
       type = "lldb",
       request = "launch",
       program = function()
