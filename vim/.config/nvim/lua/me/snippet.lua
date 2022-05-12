74e8c1b3f50b244b0c3b0ec0c787f7277de1f51b
Mathias Fussenegger <f.mathias@zignar.net>
2022-05-12 19:50:33 +0200

    Add & adjust some code snippets

--- vim/.config/nvim/lua/me/snippet.lua
+++ vim/.config/nvim/lua/me/snippet.lua
@@ -17,17 +17,23 @@ function M.setup()
       s('date', p(os.date, '%Y-%m-%d')),
     },
     lua = {
-      parse_snippet('lf', 'local function ${1:name}(${2})\n  $0\nend'),
-      parse_snippet('mf', 'function ${1:M}.${2:name}(${3})\n  $0\nend'),
+      parse_snippet('f', 'local function ${1:name}(${2})\n  $0\nend'),
+      parse_snippet('m', 'function ${1:M}.${2:name}(${3})\n  $0\nend'),
     },
     java = {
-      parse_snippet('lf', 'private ${1:static} ${2:void} ${3:name}(${4}) {\n    $0\n}'),
+      parse_snippet('f', '${1:private} ${2:static} ${3:void} ${4:name}(${5}) {\n    $0\n}'),
+      parse_snippet('m', '${1:private} ${2:void} ${3:name}(${4}) {\n    $0\n}'),
+      parse_snippet('logger', 'private static final Logger LOGGER = LogManager.getLogger(${1}.class);'),
       parse_snippet('test', '@Test\npublic void ${1:test}() {\n    $0\n}'),
     },
     ansible = {
       parse_snippet('git', 'git:\n  repo: ${1}\n  dest: ${0}\n'),
-      parse_snippet('pip', 'pip:\n  name:${1}\n  virtualenv: ${2}\n  virtualenv_command: /usr/bin/python3 -m venv\n${0}'),
+      parse_snippet('pip', 'ansible.builtin.pip:\n  name:${1}\n  virtualenv: ${2}\n  virtualenv_command: /usr/bin/python3 -m venv\n${0}'),
       parse_snippet('get_url', 'get_url:\n  url: ${1}\n  dest: ${0}\n'),
+    },
+    haskell = {
+      parse_snippet('f', '${1:name} :: ${2}\n${1:name} = ${0:undefined}'),
+      parse_snippet('lang', '{-# LANGUAGE ${1} #-}$0'),
     },
   })
 end
