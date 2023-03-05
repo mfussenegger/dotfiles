a6778103ac9cd48df8dbddb14751f20375561540
Mathias Fussenegger <f.mathias@zignar.net>
2023-03-05 08:25:11 +0100

    vim: Set root_dir for hls

--- vim/.config/nvim/ftplugin/haskell.lua
+++ vim/.config/nvim/ftplugin/haskell.lua
@@ -1,12 +1,15 @@
-vim.lsp.start(vim.tbl_extend('force', require('me.lsp').mk_config(), {
+vim.lsp.start(require('me.lsp').mk_config {
   name = 'hls',
   cmd = {'haskell-language-server-wrapper', '--lsp'},
+  root_dir = require("me.lsp").find_root({"stack.yml", ".git"}),
   on_attach = function(client, bufnr)
-    require('lsp_compl').attach(client, bufnr, { server_side_fuzzy_completion = true })
+    require('lsp_compl').attach(client, bufnr, {
+      server_side_fuzzy_completion = true
+    })
   end,
   init_options = {
     languageServerHaskell = {
       formattingProvider = "ormolu",
     }
   },
-}))
+})
