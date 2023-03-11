a32190b76b678517849d6da84d56836d44a22f2d
Mathias Fussenegger <f.mathias@zignar.net>
2023-03-11 15:11:59 +0100

    vim: Add keymap/func to run individual lua tests for neovim/neovim

--- vim/.config/nvim/ftplugin/lua.lua
+++ vim/.config/nvim/ftplugin/lua.lua
@@ -142,13 +142,80 @@ local config = lsp.mk_config {
 }
 vim.lsp.start(config)
 
-local bufnr = api.nvim_get_current_buf()
 
 if vim.loop.fs_stat("lemmy.sh") then
+  local bufnr = api.nvim_get_current_buf()
   local group = vim.api.nvim_create_augroup('lemmy-' .. bufnr, { clear = true })
   api.nvim_create_autocmd('BufWritePost', {
     command = 'silent !./lemmy.sh',
     buffer = bufnr,
     group = group
   })
+end
+
+
+if vim.endswith(config.root_dir, "neovim/neovim") then
+  local function run_test()
+    local bufnr = api.nvim_get_current_buf()
+    local lang = "lua"
+    local end_row = api.nvim_win_get_cursor(0)[1]
+    local query = vim.treesitter.parse_query(lang, [[
+      ((function_call
+        name: (identifier) @name (#any-of? @name "describe" "it")
+        arguments: (arguments
+          ((string) @str)
+        )
+      ))
+    ]])
+
+    local function get_text(root)
+      local start_row = root:range()
+      for id, node in query:iter_captures(root, bufnr, start_row, end_row) do
+        local name = query.captures[id]
+        local text = vim.treesitter.query.get_node_text(node, bufnr)
+        assert(text and type(text) == "string")
+        if name == "str" then
+          -- strip leading and trailing quotes
+          return text:sub(2, #text - 1)
+        end
+      end
+      return nil
+    end
+
+    local node = vim.treesitter.get_node()
+    local path = {}
+    while node ~= nil do
+      if node:type() == "function_call" then
+        local text = get_text(node)
+        if text then
+          table.insert(path, text)
+        end
+      end
+      node = node:parent()
+    end
+
+    for i = 1, math.floor(#path * 0.5) do
+      local tmp = path[i]
+      path[i] = path[#path - (i - 1)]
+      path[#path - (i - 1)] = tmp
+    end
+
+    vim.cmd.split()
+    local fname = api.nvim_buf_get_name(bufnr)
+    local filter = table.concat(path, " ")
+    vim.cmd.term(string.format(
+      [[TEST_FILE="%s" make functionaltest TEST_FILTER="%s"]],
+      fname,
+      filter
+    ))
+  end
+
+  vim.keymap.set("n", "<leader>dn", run_test)
+  vim.keymap.set("n", "<leader>df", function()
+    vim.cmd.split()
+    vim.cmd.term(string.format(
+      [[TEST_FILE="%s" make functionaltest]],
+      api.nvim_buf_get_name(0)
+    ))
+  end)
 end
