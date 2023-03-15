79cbd8a3d37d8a499c108f2753d080b8e8d1b256
Mathias Fussenegger <f.mathias@zignar.net>
2023-03-15 20:15:36 +0100

    vim: Run gradle-ls with default JAVA_HOME

--- vim/.config/nvim/ftplugin/gradle.lua
+++ vim/.config/nvim/ftplugin/gradle.lua
@@ -1,14 +1,12 @@
-local config = vim.tbl_extend('force', require('me.lsp').mk_config(), {
+local lsp = require("me.lsp")
+local config = lsp.mk_config {
   cmd = {
     os.getenv('HOME') .. '/dev/microsoft/vscode-gradle/build/gradle-language-server/bin/gradle-language-server'
   },
   name = 'gradle-ls',
-  cmd_env = {
-    JAVA_HOME = '/usr/lib/jvm/java-11-openjdk'
-  },
-  root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git'}, { upward = true })[1]),
+  root_dir = lsp.find_root({"gradlew", ".git"}),
   settings = {
     gradleWrapperEnabled = true,
   }
-})
+}
 vim.lsp.start(config)
