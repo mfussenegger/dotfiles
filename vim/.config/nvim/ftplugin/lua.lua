fb6e358d0f2abcd44903b8bb83e4c833f13f1681
Mathias Fussenegger <f.mathias@zignar.net>
2023-04-03 20:47:42 +0200

    vim/lua: handle deprecations: parse_query → query.parse

--- vim/.config/nvim/ftplugin/lua.lua
+++ vim/.config/nvim/ftplugin/lua.lua
@@ -157,7 +157,7 @@ local function find_test()
     local bufnr = api.nvim_get_current_buf()
     local lang = "lua"
     local end_row = api.nvim_win_get_cursor(0)[1]
-    local query = vim.treesitter.parse_query(lang, [[
+    local query = vim.treesitter.query.parse(lang, [[
       ((function_call
         name: (identifier) @name (#any-of? @name "describe" "it")
         arguments: (arguments
@@ -170,7 +170,7 @@ local function find_test()
       local start_row = root:range()
       for id, node in query:iter_captures(root, bufnr, start_row, end_row) do
         local name = query.captures[id]
-        local text = vim.treesitter.query.get_node_text(node, bufnr)
+        local text = vim.treesitter.get_node_text(node, bufnr)
         assert(text and type(text) == "string")
         if name == "str" then
           -- strip leading and trailing quotes
