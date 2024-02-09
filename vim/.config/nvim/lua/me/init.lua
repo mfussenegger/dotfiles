95e7cdd6d3c1268ebf68991932130bbc353b143b
Mathias Fussenegger <f.mathias@zignar.net>
2024-02-09 20:19:57 +0100

    vim: Update diagnostic sign config to account for upstream changes

--- vim/.config/nvim/lua/me/init.lua
+++ vim/.config/nvim/lua/me/init.lua
@@ -202,6 +202,20 @@ function M.setup()
   require('hop').setup()
 
   vim.diagnostic.config({
+    signs = {
+      text = {
+        [vim.diagnostic.severity.ERROR] = "",
+        [vim.diagnostic.severity.WARN] = "",
+        [vim.diagnostic.severity.INFO] = "",
+        [vim.diagnostic.severity.HINT] = "",
+      },
+      numhl = {
+        [vim.diagnostic.severity.ERROR] = "ErrorMsg",
+        [vim.diagnostic.severity.WARN] = "WarningMsg",
+        [vim.diagnostic.severity.INFO] = "Underlined",
+        [vim.diagnostic.severity.HINT] = "Underlined",
+      }
+    },
     virtual_text = false,
     float = {
       source = 'always',
