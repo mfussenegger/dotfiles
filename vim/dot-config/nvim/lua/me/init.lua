78fb5cfc2530ce6c8f1c3f5f46aa2590959ded49
Mathias Fussenegger <f.mathias@zignar.net>
2024-05-29 22:31:36 +0200

    vim: Drop fallback for missing vim.lsp.status

--- vim/dot-config/nvim/lua/me/init.lua
+++ vim/dot-config/nvim/lua/me/init.lua
@@ -58,42 +58,13 @@ function M.dap_status()
 end
 
 
-local function format_lsp_progress(messages)
-  local percentage
-  local result = {}
-  for _, msg in pairs(messages) do
-    if msg.message then
-      table.insert(result, msg.title .. ': ' .. msg.message)
-    else
-      table.insert(result, msg.title)
-    end
-    if msg.percentage then
-      percentage = math.max(percentage or 0, msg.percentage)
-    end
-  end
-  if percentage then
-    return string.format('%03d: %s', percentage, table.concat(result, ', '))
-  else
-    return table.concat(result, ', ')
-  end
-end
-
-
 function M.file_or_lsp_status()
   local mode = api.nvim_get_mode().mode
-  if vim.lsp.status then
-    local lsp_status = vim.lsp.status()
-    if mode ~= "n" or lsp_status == "" then
-      return M.format_uri(vim.uri_from_bufnr(api.nvim_get_current_buf()))
-    end
-    return lsp_status
-  end
-
-  local messages = vim.lsp.util.get_progress_messages()
-  if mode ~= 'n' or vim.tbl_isempty(messages) then
+  local lsp_status = vim.lsp.status()
+  if mode ~= "n" or lsp_status == "" then
     return M.format_uri(vim.uri_from_bufnr(api.nvim_get_current_buf()))
   end
-  return format_lsp_progress(messages)
+  return lsp_status
 end
 
 
