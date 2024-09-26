4eafb888619fcb91836172c34e8d1d3678539500
Mathias Fussenegger <f.mathias@zignar.net>
2024-09-26 19:31:07 +0200

    Support codeberg in gh-cd

--- bin/bin/gh-maybe-clone.hs
+++ bin/bin/gh-maybe-clone.hs
@@ -5,52 +5,70 @@
   --package process
 -}
 
+{-# LANGUAGE OverloadedRecordDot #-}
+
 import System.Environment (getArgs)
 import qualified System.Directory as D
 import qualified Network.URI as URI
 import qualified System.Process as P
 import Control.Monad (unless)
+import Data.Foldable (for_)
 
 
-createAndPrint :: String -> IO ()
-createAndPrint ('/' : tail) = do
+data ParsedPath = ParsedPath
+  { user :: String,
+    repo :: String
+  }
+  deriving (Eq, Show)
+
+
+--- >>> parse "/mfussenegger/dotfiles/"
+-- Just (ParsedPath {user = "mfussenegger", repo = "dotfiles"})
+parse :: String -> Maybe ParsedPath
+parse ('/' : tail) =
+  let user = takeWhile (/= '/') tail
+      repo = takeWhile (/= '/') . drop 1 . dropWhile (/= '/') $ tail
+  in Just ParsedPath { user, repo }
+parse _ = Nothing
+
+
+createAndPrint :: String -> ParsedPath -> IO ()
+createAndPrint prefix path = do
     home <- D.getHomeDirectory
-    let userPath = home <> "/dev/" <> user <> "/"
-        repoPath = userPath <> repo
+    let userPath = home <> "/dev/" <> path.user <> "/"
+        repoPath = userPath <> path.repo
     D.createDirectoryIfMissing True userPath
     doesExist <- D.doesDirectoryExist repoPath
-    unless doesExist (cloneRepo userPath repoIdent)
+    unless doesExist (cloneRepo prefix userPath repoIdent)
     putStrLn repoPath
   where
-    user = takeWhile (/= '/') tail
-    repo = takeWhile (/= '/') . drop 1 . dropWhile (/= '/') $ tail
-    repoIdent = user <> "/" <> repo
-createAndPrint path = putStrLn $ "Invalid path: " <> path
+    repoIdent = path.user <> "/" <> path.repo
 
 
-cloneRepo :: FilePath -> String -> IO ()
-cloneRepo dir repoIdent = do
+cloneRepo :: String -> FilePath -> String -> IO ()
+cloneRepo prefix dir repoIdent = do
     P.readCreateProcessWithExitCode proc { P.cwd = Just dir } ""
     pure ()
   where
-    repo = "gh:" <> repoIdent
+    repo = prefix <> repoIdent
     proc = P.proc "git" ["clone", repo]
 
 
 withUri :: URI.URI -> IO ()
 withUri uri =
-  if scheme == "http:" || scheme == "https:" && isGithubURI
-    then createAndPrint $ URI.uriPath uri
-    else putStrLn "No Github URI, nothing to do"
+  if scheme == "http:" || scheme == "https:"
+    then case URI.uriRegName <$> URI.uriAuthority uri of
+      Just "github.com" -> createAndPrint "gh:" parsedPath
+      Just "codeberg.org" -> createAndPrint "cb:" parsedPath
+      _ -> putStrLn "No Github URI, nothing to do"
+    else putStrLn "No HTTP URI, nothing to do"
   where
     scheme = URI.uriScheme uri
-    isGithubURI = (URI.uriRegName <$> URI.uriAuthority uri) == Just "github.com"
+    Just parsedPath = parse $ URI.uriPath uri
 
 
 main :: IO ()
 main = do
   [url] <- getArgs
   let uri = URI.parseAbsoluteURI url
-  case uri of
-    Nothing -> pure ()
-    Just uri' -> withUri uri'
+  for_ uri withUri
