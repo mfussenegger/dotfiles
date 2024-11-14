c6aa3864d5b5e195c4eb0c69bd8805ecc39170cc
Mathias Fussenegger <f.mathias@zignar.net>
2024-11-14 17:15:37 +0100

    vim: Support snippet template files

--- vim/dot-config/nvim/init.lua
+++ vim/dot-config/nvim/init.lua
@@ -168,14 +168,23 @@ create_autocmd("BufNewFile", {
   callback = function(args)
     local home = os.getenv("HOME")
     local fname = vim.fn.fnamemodify(args.file, ":t")
-    local tmpl = home .. "/.config/nvim/templates/" .. fname ..".tpl"
-    if vim.loop.fs_stat(tmpl) then
-      vim.cmd("0r " .. tmpl)
-    else
-      local ext = vim.fn.fnamemodify(args.file, ":e")
-      tmpl = home .. "/.config/nvim/templates/" .. ext ..".tpl"
-      if vim.loop.fs_stat(tmpl) then
+    local ext = vim.fn.fnamemodify(args.file, ":e")
+    local candidates = { fname, ext }
+    local uv = vim.uv
+    for _, candidate in ipairs(candidates) do
+      local tmpl = table.concat({ home, "/.config/nvim/templates/", candidate, ".tpl" })
+      if uv.fs_stat(tmpl) then
         vim.cmd("0r " .. tmpl)
+        return
+      end
+    end
+    for _, candidate in ipairs(candidates) do
+      local tmpl = table.concat({ home, "/.config/nvim/templates/", candidate, ".stpl" })
+      local f = io.open(tmpl, "r")
+      if f then
+        local content = f:read("*a")
+        vim.snippet.expand(content)
+        return
       end
     end
   end
