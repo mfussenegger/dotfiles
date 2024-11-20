4c45f5f47ecea75766796b4fa997f9eb89a46aa0
Mathias Fussenegger <f.mathias@zignar.net>
2024-11-20 20:07:58 +0100

    vim: Add :Term command
    Like `:term` but with range support
    Until https://github.com/neovim/neovim/pull/22374 gets merged

--- vim/dot-config/nvim/commands.lua
+++ vim/dot-config/nvim/commands.lua
@@ -107,3 +107,71 @@ api.nvim_create_user_command("CUniqueFile", function()
   end
   vim.fn.setqflist(qflist, "r")
 end, { nargs = 0 })
+
+
+api.nvim_create_user_command("Term", function(args)
+  local srcbuf = api.nvim_get_current_buf()
+  local lines
+  if args.range ~= 0 then
+    lines = api.nvim_buf_get_lines(srcbuf, args.line1 -1 , args.line2, true)
+    local indent = math.huge
+    for _, line in ipairs(lines) do
+      indent = math.min(line:find("[^ ]") or math.huge, indent)
+    end
+    if indent ~= math.huge and indent > 0 then
+      for i, line in ipairs(lines) do
+        lines[i] = line:sub(indent)
+      end
+    end
+  end
+  vim.cmd("belowright split")
+  local win = api.nvim_get_current_win()
+  vim.wo[win].number = false
+  vim.wo[win].relativenumber = false
+  vim.wo[win].signcolumn = "no"
+  local bufnr = api.nvim_create_buf(true, true)
+  api.nvim_win_set_buf(win, bufnr)
+  local jobid
+  local chan = api.nvim_open_term(bufnr, {
+    on_input = function(_, _, _, data)
+      pcall(api.nvim_chan_send, jobid, data)
+    end
+  })
+  local opts = {
+    height = api.nvim_win_get_height(win),
+    width = api.nvim_win_get_width(win),
+    pty = true,
+    on_stdout = function(_, data)
+      local count = #data
+      for idx, line in ipairs(data) do
+        local islast = idx == count
+        local msg = islast and line or (line .. '\n')
+        local send_ok = pcall(api.nvim_chan_send, chan, msg)
+        if not send_ok then
+          return
+        end
+      end
+    end,
+    on_exit = function(_, exit_code)
+      pcall(api.nvim_chan_send, chan, '\r\n[Process exited ' .. tostring(exit_code) .. ']')
+      pcall(api.nvim_buf_set_keymap, bufnr, "t", "<CR>", "<cmd>bd!<CR>", { noremap = true, silent = true})
+    end,
+  }
+  local cmd = args.args == "" and vim.o.shell or args.args
+  if lines then
+    local inputs = vim.fn.tempname()
+    vim.fn.system(string.format('mkfifo "%s"', inputs))
+    jobid = vim.fn.jobstart(string.format('cat "%s" | %s', inputs, cmd), opts)
+    local f = io.open(inputs, "a")
+    if f then
+      for _, line in ipairs(lines) do
+        f:write(line)
+        f:write("\n")
+      end
+      f:flush()
+      f:close()
+    end
+  else
+    jobid = vim.fn.jobstart(cmd, opts)
+  end
+end, { nargs = "*", range = "%", desc = "Like :term but supporting range" })
