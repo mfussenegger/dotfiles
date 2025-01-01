740b955bfd65b155af131f3e0a862450ea8b8bee
Mathias Fussenegger <f.mathias@zignar.net>
2025-01-01 15:16:36 +0100

    vim: try lsp.config/enable

--- vim/dot-config/nvim/lua/me/lsp.lua
+++ vim/dot-config/nvim/lua/me/lsp.lua
@@ -1,12 +1,10 @@
-local lsp = require 'vim.lsp'
-local api = vim.api
-local ms = lsp.protocol.Methods
 local M = {}
+local lsp = vim.lsp
+local api = vim.api
 
----@diagnostic disable-next-line: deprecated
-local get_clients = vim.lsp.get_clients or vim.lsp.get_active_clients
 
-
+---@param config vim.lsp.ClientConfig
+---@return vim.lsp.ClientConfig
 function M.mk_config(config)
   local lsp_compl = require('lsp_compl')
   local capabilities = vim.tbl_deep_extend(
@@ -22,32 +20,8 @@ function M.mk_config(config)
     }
   )
   local defaults = {
-    flags = {
-      debounce_text_changes = 80,
-    },
     handlers = {},
     capabilities = capabilities,
-    on_attach = function(client, bufnr)
-      local triggers = vim.tbl_get(client.server_capabilities, "completionProvider", "triggerCharacters")
-      if triggers then
-        for _, char in ipairs({"a", "e", "i", "o", "u"}) do
-          if not vim.tbl_contains(triggers, char) then
-            table.insert(triggers, char)
-          end
-        end
-        for i, t in ipairs(triggers) do
-          if t == "," then
-            triggers[i] = nil
-          end
-        end
-        client.server_capabilities.completionProvider.triggerCharacters = vim.iter(triggers):totable()
-      end
-      if vim.lsp.completion then
-        vim.lsp.completion.enable(true, client.id, bufnr, { autotrigger = true })
-      else
-        lsp_compl.attach(client, bufnr)
-      end
-    end,
     init_options = vim.empty_dict(),
     settings = vim.empty_dict(),
   }
@@ -59,166 +33,205 @@ function M.mk_config(config)
 end
 
 
-function M.setup()
-  vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, { border = 'single' })
-  vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = 'single' })
-  local servers = {
-    {'html', {'vscode-html-language-server', '--stdio'}},
-    {'htmldjango', {'vscode-html-language-server', '--stdio'}},
-    {'json', {'vscode-json-language-server', '--stdio'}},
-    {'css', {'vscode-css-language-server', '--stdio'}},
-    {'c', 'clangd', {'.git'}},
-    {'cpp', 'clangd', {'.git'}},
-    {'sh', {'bash-language-server', 'start'}},
-    {'rust', 'rust-analyzer', {'Cargo.toml', '.git'}},
-    {'tex', 'texlab', {'.git'}},
-    {'javascript', {'typescript-language-server', '--stdio'}, {"package.json", ".git"}},
-    {'typescript', {'typescript-language-server', '--stdio'}, {"package.json", ".git"}},
-  }
-  local lsp_group = api.nvim_create_augroup('lsp', {})
-  for _, server in pairs(servers) do
-    api.nvim_create_autocmd('FileType', {
-      pattern = server[1],
-      group = lsp_group,
+---@param config vim.lsp.Config
+local function enable(name, config)
+  if lsp.config then
+    lsp.config(name, config)
+    lsp.enable(name)
+    return
+  end
+  local group = api.nvim_create_augroup("lsp-enable-" .. name, { clear = true })
+  for _, ft in ipairs(config.filetypes) do
+    api.nvim_create_autocmd("FileType", {
+      pattern = ft,
+      group = group,
+
+      ---@param args vim.api.keyset.create_autocmd.callback_args
       callback = function(args)
-        local cmd = server[2]
-        local config = M.mk_config({
-          name = type(cmd) == "table" and cmd[1] or cmd,
-          cmd = type(cmd) == "table" and cmd or {cmd},
-        })
-        local markers = server[3]
-        if markers then
-          config.root_dir = vim.fs.root(args.file, markers)
+        if config.root_markers then
+          config = vim.deepcopy(config)
+          config.root_dir = vim.fs.root(args.buf, config.root_markers)
         end
-        vim.lsp.start(config)
+        vim.lsp.start(config, {
+          bufnr = args.buf,
+          reuse_client = config.reuse_client,
+        })
       end,
     })
   end
-  if vim.fn.exists('##LspAttach') ~= 1 then
+end
+
+---@param client vim.lsp.Client
+local function extendtriggers(client)
+  local triggers = vim.tbl_get(client.server_capabilities, "completionProvider", "triggerCharacters")
+  if not triggers then
     return
   end
-  local keymap = vim.keymap
-  if vim.fn.exists("##LspProgress") == 1 then
-    local timer = vim.loop.new_timer()
-    api.nvim_create_autocmd("LspProgress", {
-      group = lsp_group,
-      callback = function()
-        if api.nvim_get_mode().mode == "n" then
-          vim.cmd.redrawstatus()
-        end
-        if timer then
-          timer:stop()
-          timer:start(500, 0, vim.schedule_wrap(function()
-            timer:stop()
-            vim.cmd.redrawstatus()
-          end))
-        end
-      end
-    })
+  for _, char in ipairs({"a", "e", "i", "o", "u"}) do
+    if not vim.tbl_contains(triggers, char) then
+      table.insert(triggers, char)
+    end
   end
-  api.nvim_create_autocmd('LspAttach', {
-    group = lsp_group,
+  for i, t in ipairs(triggers) do
+    if t == "," then
+      triggers[i] = nil
+    end
+  end
+  client.server_capabilities.completionProvider.triggerCharacters = vim.iter(triggers):totable()
+end
+
+
+function M.setup()
+  enable("json-ls", {
+    cmd = {"vscode-json-language-server", "--stdio"},
+    filetypes = {"json"}
+  })
+  enable("html-ls", {
+    cmd = {"vscode-html-language-server", "--stdio"},
+    filetypes = {"html", "htmldjango"}
+  })
+  enable("css-ls", {
+    cmd = {"vscode-css-language-server", "--stdio"},
+    filetypes = {"css"}
+  })
+  enable("bash-ls", {
+    cmd = {"bash-language-server", "start"},
+    filetypes = {"sh"}
+  })
+  enable("ts-ls", {
+    cmd = {"typescript-language-server", "--stdio"},
+    filetypes = {"javascript", "typescript"},
+    root_markers = {"package.json", ".git"},
+  })
+
+  local hover = vim.lsp.buf.hover
+  ---@diagnostic disable-next-line: duplicate-set-field
+  vim.lsp.buf.hover = function(config)
+    config = config or {}
+    config.border = "single"
+    hover(config)
+  end
+
+  local signature_help = vim.lsp.buf.signature_help
+  ---@diagnostic disable-next-line: duplicate-set-field
+  vim.lsp.buf.signature_help = function(config)
+    config = config or {}
+    config.border = "single"
+    signature_help(config)
+  end
+
+  local setk = vim.keymap.set
+  setk("n", "gr", function() vim.lsp.buf.references({ includeDeclaration = false }) end)
+  setk({"n", "x"}, "<a-CR>", vim.lsp.buf.code_action)
+  setk({"n", "x"}, "<leader>r", "<Cmd>lua vim.lsp.buf.code_action { context = { only = {'refactor'} }}<CR>")
+  setk("n", "crn", "<Cmd>lua vim.lsp.buf.rename(vim.fn.input('New Name: '))<CR>")
+  setk("n", "<leader>cr", function() vim.lsp.codelens.refresh({ bufnr = 0 }) end)
+  setk("n", "<leader>ce", vim.lsp.codelens.run)
+
+  local lspgroup = api.nvim_create_augroup("lsp", {})
+  api.nvim_create_autocmd("LspAttach", {
+    group = lspgroup,
+    ---@param args vim.api.keyset.create_autocmd.callback_args
     callback = function(args)
-      -- array of mappings to setup; {<capability>, <mode>, <lhs>, <rhs>}
-      local key_mappings = {
-        {"referencesProvider", "n", "gr",
-          function()
-            vim.lsp.buf.references({ includeDeclaration = false })
-          end
-        },
-        {"implementationProvider", "n", "gD",  vim.lsp.buf.implementation},
-        {"signatureHelpProvider", "i", "<c-space>", vim.lsp.buf.signature_help},
-        {"codeLensProvider", "n", "<leader>cr", function() vim.lsp.codelens.refresh({bufnr = 0}) end},
-        {"codeLensProvider", "n", "<leader>ce", vim.lsp.codelens.run},
-        {"codeLensProvider", "n", "<leader>ca",
-          function()
-            vim.lsp.codelens.refresh({ bufnr = 0 })
-            local bufnr = api.nvim_get_current_buf()
-            api.nvim_create_autocmd({'InsertLeave', 'CursorHold'}, {
-              group = api.nvim_create_augroup(string.format('lsp-codelens-%s', bufnr), {}),
-              buffer = bufnr,
-              callback = function()
-                vim.lsp.codelens.refresh({ bufnr = 0 })
-              end,
-            })
-          end
-        },
-        {"codeLensProvider", "n", "<leader>cc",
-          function()
-            local bufnr = api.nvim_get_current_buf()
-            if vim.lsp.codelens.clear then
-              vim.lsp.codelens.clear(nil, bufnr)
-            end
-            local group = string.format('lsp-codelens-%s', bufnr)
-            pcall(api.nvim_del_augroup_by_name, group)
-          end
-        },
-      }
-
-      keymap.set({"n", "v"}, "<a-CR>", vim.lsp.buf.code_action, { buffer = args.buf })
-      keymap.set("n", "<leader>r", "<Cmd>lua vim.lsp.buf.code_action { context = { only = {'refactor'} }}<CR>", { buffer = args.buf })
-      keymap.set("v", "<leader>r", "<Cmd>lua vim.lsp.buf.code_action { context = { only = {'refactor'}}}<CR>", { buffer = args.buf })
-
       local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-      keymap.set("n", "crn", "<Cmd>lua vim.lsp.buf.rename(vim.fn.input('New Name: '))<CR>", { buffer = args.buf })
-      keymap.set("i", "<c-n>", function()
-        if vim.lsp.completion then
-          vim.lsp.completion.trigger()
-        else
-          require("lsp_compl").trigger_completion()
-        end
-      end, { buffer = args.buffer })
-      keymap.set('i', '<CR>', function()
-        return require('lsp_compl').accept_pum() and '<c-y>' or '<CR>'
-      end, { expr = true, buffer = args.buffer })
 
-      for _, mappings in pairs(key_mappings) do
-        local capability, mode, lhs, rhs = unpack(mappings)
-        if client.server_capabilities[capability] then
-          keymap.set(mode, lhs, rhs, { buffer = args.buf, silent = true })
+      if client.server_capabilities.completionProvider then
+        extendtriggers(client)
+        if vim.lsp.completion then
+          vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+        else
+          require("lsp_compl").attach(client, args.buf, {})
         end
       end
+      if client.server_capabilities.implementationProvider then
+        setk("n", "gD", vim.lsp.buf.implementation, { buffer = args.buf })
+      end
+
+      if client.server_capabilities.codeLensProvider then
+        local bufnr = args.buf
+        local cl_group_name = string.format("lsp-codelens-%d", bufnr)
+
+        local function autorefresh()
+          vim.lsp.codelens.refresh({ bufnr = bufnr })
+          api.nvim_create_autocmd({"InsertLeave", "CursorHold"}, {
+            group = api.nvim_create_augroup(cl_group_name, { clear = true }),
+            buffer = bufnr,
+            callback = function()
+              vim.lsp.codelens.refresh({ bufnr = bufnr })
+            end,
+          })
+        end
+
+        local function clear()
+          if vim.lsp.codelens.clear then
+            vim.lsp.codelens.clear(nil, bufnr)
+          end
+          pcall(api.nvim_del_augroup_by_name, cl_group_name)
+        end
+
+        setk("n", "<leader>ca", autorefresh, { buffer = bufnr })
+        setk("n", "<leader>cc", clear, { buffer = bufnr })
+      end
+
       if client.server_capabilities.documentHighlightProvider then
-        local group = api.nvim_create_augroup(string.format('lsp-%s-%s', args.buf, args.data.client_id), {})
-        api.nvim_create_autocmd({'CursorHold', 'CursorHoldI'}, {
+        local group = api.nvim_create_augroup(string.format("lsp-%s-%s", args.buf, args.data.client_id), {})
+        api.nvim_create_autocmd("CursorHold", {
           group = group,
           buffer = args.buf,
           callback = function()
             local params = vim.lsp.util.make_position_params(0, client.offset_encoding)
-            client.request('textDocument/documentHighlight', params, nil, args.buf)
+            ---@diagnostic disable-next-line: param-type-mismatch
+            client.request("textDocument/documentHighlight", params, nil, args.buf)
           end,
         })
-        api.nvim_create_autocmd('CursorMoved', {
+        api.nvim_create_autocmd("CursorMoved", {
           group = group,
           buffer = args.buf,
           callback = function()
-            pcall(vim.lsp.buf.clear_references)
+            pcall(vim.lsp.util.buf_clear_references, args.buf)
           end,
         })
       end
     end,
   })
-  api.nvim_create_autocmd('LspDetach', {
-    group = lsp_group,
+  api.nvim_create_autocmd("LspDetach", {
+    group = lspgroup,
     callback = function(args)
-      local group = api.nvim_create_augroup(string.format('lsp-%s-%s', args.buf, args.data.client_id), {})
+      local group = api.nvim_create_augroup(string.format("lsp-%s-%s", args.buf, args.data.client_id), {})
       pcall(api.nvim_del_augroup_by_name, group)
-      pcall(require('lsp_compl').detach, args.data.client_id, args.buf)
     end,
   })
+
+  local timer = vim.loop.new_timer()
+  api.nvim_create_autocmd("LspProgress", {
+    group = lspgroup,
+    callback = function()
+      if api.nvim_get_mode().mode == "n" then
+        vim.cmd.redrawstatus()
+      end
+      if timer then
+        timer:stop()
+        timer:start(500, 0, vim.schedule_wrap(function()
+          timer:stop()
+          vim.cmd.redrawstatus()
+        end))
+      end
+    end,
+  })
+
 
   api.nvim_create_user_command(
     'LspStop',
     function(kwargs)
       local name = kwargs.fargs[1]
-      for _, client in ipairs(get_clients({ name = name })) do
-        client.stop()
+      for _, client in ipairs(lsp.get_clients({ name = name })) do
+        client:stop()
       end
     end,
     {
-      nargs = 1,
+      nargs = "?",
       complete = function()
-        return vim.tbl_map(function(c) return c.name end, get_clients())
+        return vim.tbl_map(function(c) return c.name end, lsp.get_clients())
       end
     }
   )
@@ -226,13 +239,13 @@ function M.setup()
     "LspRestart",
     function(kwargs)
       local name = kwargs.fargs[1]
-      for _, client in ipairs(get_clients({ name = name })) do
+      for _, client in ipairs(lsp.get_clients({ name = name })) do
         local bufs = lsp.get_buffers_by_client_id(client.id)
-        client.stop()
+        client:stop()
         vim.wait(30000, function()
           return lsp.get_client_by_id(client.id) == nil
         end)
-        local client_id = lsp.start_client(client.config)
+        local client_id = lsp.start(client.config, { attach = false })
         if client_id then
           for _, buf in ipairs(bufs) do
             lsp.buf_attach_client(buf, client_id)
@@ -243,7 +256,7 @@ function M.setup()
     {
       nargs = "?",
       complete = function()
-        return vim.tbl_map(function(c) return c.name end, get_clients())
+        return vim.tbl_map(function(c) return c.name end, lsp.get_clients())
       end
     }
   )
@@ -251,3 +264,4 @@ end
 
 
 return M
+
