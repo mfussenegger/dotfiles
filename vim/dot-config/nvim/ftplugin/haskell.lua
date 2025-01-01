870d9a0670f19b0e79ab9ea34f94cc0fb75da14c
Mathias Fussenegger <f.mathias@zignar.net>
2025-01-01 14:51:03 +0100

    vim/haskell: Disable semantic tokens

--- vim/dot-config/nvim/ftplugin/haskell.lua
+++ vim/dot-config/nvim/ftplugin/haskell.lua
@@ -2,17 +2,13 @@ vim.lsp.start(require('me.lsp').mk_config {
   name = 'hls',
   cmd = {'haskell-language-server-wrapper', '--lsp'},
   root_dir = vim.fs.root(0, {"stack.yml", ".git"}),
-  on_attach = function(client, bufnr)
-    if vim.lsp.completion then
-      vim.lsp.completion.enable(true, client.id, bufnr, { autotrigger = true })
-    else
-      require('lsp_compl').attach(client, bufnr, {
-        server_side_fuzzy_completion = true
-      })
-    end
+
+  ---@param client vim.lsp.Client
+  on_attach = function(client)
+    client.server_capabilities.semanticTokensProvider = nil
   end,
-  init_options = {
-    languageServerHaskell = {
+  settings = {
+    haskell = {
       formattingProvider = "ormolu",
     }
   },
