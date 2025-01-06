6ec604606ba9d7cb9659e83f0433e77c0c26fe0b
Mathias Fussenegger <f.mathias@zignar.net>
2025-01-06 10:52:48 +0100

    zsh: Remove unused aliases

--- shell/.zsh/aliases.plugin.zsh
+++ shell/.zsh/aliases.plugin.zsh
@@ -4,7 +4,6 @@ alias la='ll -A'
 alias lt='ll -tr'
 alias lu='lt -u'
 
-alias fpacman="pacman -Slq | fzf -m --preview 'pacman -Si {1}' | xargs -ro sudo pacman -S"
 alias p='sudo pacman'
 alias sc='systemctl'
 
@@ -24,20 +23,10 @@ alias vgdisplay='sudo vgdisplay'
 alias zh='z -I -t .'
 alias zb='z -b'
 
-
-## yay python smtpd
-alias smtpd="python -m smtpd -n -c DebuggingServer localhost:1025"
-
-
 ## global aliases
-alias -g L='| less'
 alias -g N='2> /dev/null'
 alias -g G='| rg'
 alias -g J='| jless'
-alias -g H='| head'
-alias -g T='| tail'
-alias -g F='| fzf'
-alias -g X='| xargs'
 alias -g V='| v -'
 if [ -n "$SSH_TTY" ] && hash tty-copy 2> /dev/null; then
   alias -g C='| tty-copy -n'
@@ -60,8 +49,5 @@ alias rsync="ionice -c 3 rsync"
 alias iostat="iostat -mtx"
 alias rdesktop="rdesktop -k de -K -z"
 alias ip="ip --color"
-
-alias benice="renice -n 19 -p $$ && ionice -c3 -p $$ && ionice -p $$"
-alias berude="sudo renice -n -19 -p $$ && sudo ionice -c1 -p $$ && ionice -p $$"
 
 alias tablet="scrcpy --v4l2-sink=/dev/video7 --no-display"
