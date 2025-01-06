fa827b77f354b0f31a8352a27cfc1d9a4973a31c
Mathias Fussenegger <f.mathias@zignar.net>
2025-01-06 18:21:54 +0100

    vim: Remove :Lua user command
    :DapEval + nluarepl does more or less the same but has a more convenient
    way to inspect the result

--- vim/dot-config/nvim/commands.lua
+++ vim/dot-config/nvim/commands.lua
@@ -49,52 +49,6 @@ api.nvim_create_user_command("Osv", function(args)
 end, { nargs = "?" })
 
 
-api.nvim_create_user_command("Lua", function(args)
-  local srcbuf = api.nvim_get_current_buf()
-  vim.cmd.split()
-  local bufnr = api.nvim_create_buf(true, true)
-  api.nvim_buf_set_name(bufnr, "lua://" .. tostring(bufnr))
-  if args.range ~= 0 then
-    local lines = api.nvim_buf_get_lines(srcbuf, args.line1 -1 , args.line2, true)
-    local indent = math.huge
-    for _, line in ipairs(lines) do
-      indent = math.min(line:find("[^ ]") or math.huge, indent)
-    end
-    if indent ~= math.huge and indent > 0 then
-      for i, line in ipairs(lines) do
-        lines[i] = line:sub(indent)
-      end
-    end
-    api.nvim_buf_set_lines(bufnr, 0, -1, true, lines)
-    vim.bo[bufnr].modified = false
-  end
-  vim.bo[bufnr].buftype = "acwrite"
-  vim.bo[bufnr].bufhidden = "wipe"
-  vim.bo[bufnr].filetype = "lua"
-  local function execlua(buf)
-    local lines = api.nvim_buf_get_lines(buf, 0, -1, true)
-    local code = table.concat(lines, "\n")
-    local fn, err = loadstring(code)
-    if fn then
-      local result = fn()
-      if result then
-        vim.print(result)
-      end
-    elseif err then
-      vim.notify(err, vim.log.levels.WARN)
-    end
-  end
-  api.nvim_set_current_buf(bufnr)
-  api.nvim_create_autocmd("BufWriteCmd", {
-    buffer = bufnr,
-    callback = function(write_args)
-      vim.bo[write_args.buf].modified = false
-      execlua(write_args.buf)
-    end,
-  })
-end, { range = "%" })
-
-
 api.nvim_create_user_command("Term", function(args)
   local srcbuf = api.nvim_get_current_buf()
   local lines
