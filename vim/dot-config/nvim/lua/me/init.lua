f2d483cfae273eb878ac31b88b64bfd659fa780a
Mathias Fussenegger <f.mathias@zignar.net>
2025-01-26 20:28:00 +0100

    Fix some minor luals warnings

--- vim/dot-config/nvim/lua/me/init.lua
+++ vim/dot-config/nvim/lua/me/init.lua
@@ -106,7 +106,7 @@ function M.only()
   local cur_buf = api.nvim_get_current_buf()
   for _, buf in ipairs(api.nvim_list_bufs()) do
     if cur_buf ~= buf then
-      pcall(vim.cmd, 'bd ' .. buf)
+      pcall(vim.cmd.bd, buf)
     end
   end
 end
@@ -186,11 +186,11 @@ function M.setup()
     },
     virtual_text = false,
     float = {
-      source = 'always',
+      source = true,
     },
   })
 
-  U = M
+  _G.U = M
 end
 
 
